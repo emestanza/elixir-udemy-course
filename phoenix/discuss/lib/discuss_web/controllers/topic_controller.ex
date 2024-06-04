20ceb9db8183d671d17fba9ff2ddc025f831cb6d
Enmanuel Mestanza <enmanuel.mestanza@zubale.com>
2024-06-03 21:35:54 -0500

    Lesson 10 - form and redirect workflow

--- phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,16 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  # use DiscussWeb, :html
+  # use DiscussWeb, :model
 
   alias DiscussWeb.Models.Topic
+  alias Discuss.Repo
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+
+    render(conn, :index, layout: {DiscussWeb.Layouts, "app.html"}, topics: topics)
+  end
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
@@ -12,8 +21,16 @@ defmodule DiscussWeb.TopicController do
   def create(conn, %{"topic" => topic} = params) do
     IO.inspect(params)
 
-    changeset = Topic.changeset(%Topic{}, %{})
+    changeset = Topic.changeset(%Topic{}, topic)
 
-    render(conn, :new, layout: {DiscussWeb.Layouts, "app.html"}, changeset: changeset)
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic created successfully")
+        |> redirect(to: ~p"/")
+
+      {:error, changeset} ->
+        render(conn, :new, layout: {DiscussWeb.Layouts, "app.html"}, changeset: changeset)
+    end
   end
 end
