1e81c69b0a2b735ee77147e3715c89b85fe6894a
Enmanuel Mestanza <enmanuel.mestanza@zubale.com>
2024-06-04 21:29:45 -0500

    Lesson 10 - update endpoint working

--- phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,5 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
-  # use DiscussWeb, :html
-  # use DiscussWeb, :model
 
   alias DiscussWeb.Models.Topic
   alias Discuss.Repo
@@ -31,6 +29,37 @@ defmodule DiscussWeb.TopicController do
 
       {:error, changeset} ->
         render(conn, :new, layout: {DiscussWeb.Layouts, "app.html"}, changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get!(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render(conn, :edit,
+      layout: {DiscussWeb.Layouts, "app.html"},
+      topic: topic,
+      changeset: changeset,
+      topic_id: topic_id
+    )
+  end
+
+  def update(conn, %{"topic" => topic_edited, "id" => topic_id} = params) do
+    old_topic = Repo.get!(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic_edited)
+
+    case Repo.update(changeset) do
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic updated successfully")
+        |> redirect(to: ~p"/")
+
+      {:error, changeset} ->
+        render(conn, :edit,
+          layout: {DiscussWeb.Layouts, "app.html"},
+          topic: topic_edited,
+          changeset: changeset
+        )
     end
   end
 end
